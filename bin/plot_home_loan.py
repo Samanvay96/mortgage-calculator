b2bf9b83785aea117a1d14038432af7eaa2321db
Samanvay Karambhe <samanvaykarambhe@gmail.com>
2017-12-26 21:52:55 +1000

    Make plot file into a class

--- bin/plot_home_loan.py
+++ bin/plot_home_loan.py
@@ -1,19 +1,29 @@
 import home_loan as hl 
 
-def plot_repayments(hl. p):
-    xValues = hl.create_array(p)
-    yValues = [hl.loan_balance(p) for p in xValues]
-    hl.make_plot(xValues, yValues, 'Debt Value ($)', 'Time (Months)', 'Debt Repayment')
+class HomeLoan:
 
-def plot_cummulative_interest(hl.p):
-    xValues = hl.create_array(p)
-    yValues = [hl.cummulative_interest(p) for p in xValues]
-    hl.make_plot(xValues, yValues, 'Cummulative Interest', 'Time (Months)', 'Interest Paid')
+    def __init__(self, principal, interest_rate, loan_period):
+        self.principal = principal
+        self.i = interest_rate
+        self.loan_period = loan_period
+        self.p = loan_period * 12
 
-def make_plot(hl. x, y, xlabel, ylabel, title):
-    plt.plot(xValues, yValues)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.show()
+    def plot_repayments(self):
+        xValues = hl.create_array(self.p)
+        yValues = [hl.loan_balance(self.principal, self.i, self.loan_period, p) for p in xValues]
+        hl.make_plot(xValues, yValues, 'Debt Value ($)', 'Time (Months)', 'Debt Repayment')
+    
+    def plot_cummulative_interest(self):
+        xValues = hl.create_array(self.p)
+        yValues = [hl.cummulative_interest(self.principal, self.i, self.loan_period, p) for p in xValues]
+        hl.make_plot(xValues, yValues, 'Cummulative Interest', 'Time (Months)', 'Interest Paid')
+    
+    def make_plot(x, y, xlabel, ylabel, title):
+        plt.plot(x, y)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.show()
 
+if __name__ == "__main__": 
+    HomeLoan(500000, 0.0465, 30).plot_repayments()
